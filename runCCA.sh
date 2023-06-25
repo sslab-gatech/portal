4cd288e4c78b9f88a6a2dbab666f77f88912d759
jaehyuk <jhl9105@gmail.com>
2023-06-24 20:41:12 -0400

    testing pci device

--- runCCA.sh
+++ runCCA.sh
@@ -1,7 +1,11 @@
 ./qemu/build/qemu-system-aarch64 -s -S -nographic -serial telnet::54340,server \
   -smp clusters=2,cores=4 -machine virt,secure=on,rmm=on,virtualization=on,gic-version=3,iommu=smmuv3 \
   -m 2048 -cpu max,lpa2=off -d unimp,guest_errors \
-  -kernel out/bin/Image -initrd rootfs.cpio.gz -bios flash.bin \
+  -device virtio-gpu-pci \
+  -kernel out/bin/Image -initrd initrd -bios flash.bin \
+#  -hda ./image.qcow2 \
+  -device nvme,drive=./image.qcow2,serial=deadbeaf1,num_queues=8 
+  #-drive file=./image.qcow2,format=ext4\
 
 #./qemu/build/qemu-system-aarch64 -s -S -nographic -serial telnet::54340,server \
 #  -smp clusters=2,cores=4 -machine virt,secure=on,rmm=on,virtualization=on,gic-version=3 \
