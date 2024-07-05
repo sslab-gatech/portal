d000b836f9d8c84d3c26ab90741d3fa61e79cd44
jaehyuk <jhl9105@gmail.com>
2024-07-05 10:43:12 -0400

    x

--- arch/arm64/kvm/vgic/vgic.c
+++ arch/arm64/kvm/vgic/vgic.c
@@ -100,7 +100,7 @@ struct vgic_irq *vgic_get_irq(struct kvm *kvm, struct kvm_vcpu *vcpu,
 
 	/* SPIs */
 	if (intid < (kvm->arch.vgic.nr_spis + VGIC_NR_PRIVATE_IRQS)) {
-		pr_info("[HOST]: Shared IRQ %d should be retrieved from spis!\n", intid);
+		//pr_info("[HOST]: Shared IRQ %d should be retrieved from spis!\n", intid);
 		intid = array_index_nospec(intid, kvm->arch.vgic.nr_spis + VGIC_NR_PRIVATE_IRQS);
 		return &kvm->arch.vgic.spis[intid - VGIC_NR_PRIVATE_IRQS];
 	}
